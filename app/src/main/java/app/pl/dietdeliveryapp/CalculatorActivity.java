0ccd876eacd9ddc2234a1cb5fd97512666795c8a
Natalia Bąkowska <nbakowska5@gmail.com>
2018-12-30 20:37:26 +0100

    calculator impl

--- app/src/main/java/app/pl/dietdeliveryapp/CalculatorActivity.java
+++ app/src/main/java/app/pl/dietdeliveryapp/CalculatorActivity.java
@@ -2,12 +2,52 @@ package app.pl.dietdeliveryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-public class CalculatorActivity extends AppCompatActivity {
+public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener {
+
+    protected EditText etAge;
+    protected EditText etHeight;
+    protected EditText etWeight;
+    protected Spinner spinner;
+    protected CheckBox cbLoss;
+    protected Button bSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        etAge = findViewById(R.id.etAge);
+        etAge.setOnClickListener(this);
+
+        etHeight = findViewById(R.id.etHeight);
+        etHeight.setOnClickListener(this);
+
+        etWeight = findViewById(R.id.etWeight);
+        etWeight.setOnClickListener(this);
+
+        spinner = findViewById(R.id.spinner);
+//        spinner.setOnItemClickListener(this);
+
+        cbLoss = findViewById(R.id.cbLoss);
+        cbLoss.setOnClickListener(this);
+
+        bSubmit = findViewById(R.id.bSubmit);
+        bSubmit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String age = etAge.getText().toString();
+        String height = etHeight.getText().toString();
+        String weight = etWeight.getText().toString();
+        String gender = spinner.getSelectedItem().toString();
+        boolean ifWeightLoss = cbLoss.isChecked();
+
     }
 }
--- app/src/main/java/app/pl/dietdeliveryapp/MainActivity.java
+++ app/src/main/java/app/pl/dietdeliveryapp/MainActivity.java
@@ -1,13 +1,59 @@
 package app.pl.dietdeliveryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    protected Button bOrder;
+    protected Button bDelivery;
+    protected Button button3;
+    protected Button bContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bOrder = findViewById(R.id.bOrder);
+        bOrder.setOnClickListener(this);
+
+        bDelivery = findViewById(R.id.bDelivery);
+        bDelivery.setOnClickListener(this);
+
+        button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+
+        bContact = findViewById(R.id.bContact);
+        bContact.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch ( id ) {
+            case R.id.bOrder:
+                Intent intent1 = new Intent(this , CalculatorActivity.class );
+                startActivity(intent1);
+                break;
+
+            case R.id.bDelivery:
+                Intent intent2 = new Intent(this , DeliveryActivity.class );
+                startActivity(intent2);
+                break;
+
+            case R.id.button3:
+                Intent intent3 = new Intent(this , WelcomeActivity.class );
+                startActivity(intent3);
+                break;
+
+            case R.id.bContact:
+                Intent intent4 = new Intent(this , ContactActivity.class );
+                startActivity(intent4);
+                break;
+        }
     }
 }
